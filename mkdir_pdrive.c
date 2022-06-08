029ae46c2eba925a4d98a10b64a32c1966d655bd
Jeh NegãO <84611374+JefersonDev@users.noreply.github.com>
2022-06-07 22:01:14 -0300

    Update mkdir_pdrive.c

--- mkdir_pdrive.c
+++ mkdir_pdrive.c
@@ -1,11 +1,3 @@
-/*
-Módulo criado para aula de SO II - Professor: Rossano
-Anisio Rodrigues da Silva Junior
-Vitor Marcolin
-Daniel Evaldo da Silva
-Beatriz Camargo
-*/
-
 #include <dirent.h>
 #include <stdio.h>
 #include <fcntl.h>
